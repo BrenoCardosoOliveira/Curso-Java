df9d03c4a703c701d5b66eedbeb4113ad9d64b46
Breno <breno.c.ow@gmail.com>
2024-04-17 19:28:32 -0300

    Exercício 1

---
+++ Curso_Java/src/exercicios/Exercio1.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Exercio1 {
+	// Exercício 1 - Preço por m²
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+	
+		Scanner sc = new Scanner(System.in);
+		double largura = sc.nextDouble();
+		double comprimento = sc.nextDouble();
+		double metroQuadrado = sc.nextDouble();
+		
+		double area = largura * comprimento;
+		double preco = area * metroQuadrado;
+		
+		System.out.printf("Area = %.2f%n", area );
+		System.out.printf("Preço = %.2f%n", preco);
+		
+		sc.close();
+
+
+	}
+
+}
